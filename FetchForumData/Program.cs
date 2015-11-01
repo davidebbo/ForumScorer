0a0e836439ef7eff9232b9eb682e9d43bf83bc5f
davidebbo <david.ebbo@microsoft.com>
2015-11-01 09:25:45 -0800

    Add initial WebJob

---
+++ FetchForumData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FetchForumData
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string usersFile = Path.GetFullPath(@"..\..\..\ForumScorer\App_Data\users.txt");
+            if (!File.Exists(usersFile))
+            {
+                Console.WriteLine($"Can't find users file '{usersFile}'.");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
