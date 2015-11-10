bd818f21d3fa5083a60114568f9f7b4c89c788d3
davidebbo <david.ebbo@microsoft.com>
2015-11-10 12:43:20 -0800

    Share Json model between input file and score file + refactoring

--- FetchForumData/Program.cs
+++ FetchForumData/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using ForumModels;
 using Newtonsoft.Json;
 
 namespace FetchForumData
@@ -24,101 +25,17 @@ namespace FetchForumData
                 }
             }
 
-            string usersFile = Path.Combine(appDataFolder, "users.txt");
-            var scores = File.ReadAllLines(usersFile).Select(u => GetUserData(u));
+            string usersFile = Path.Combine(appDataFolder, "users.json");
+            string userJson = File.ReadAllText(usersFile);
+            var users = JsonConvert.DeserializeObject<List<User>>(userJson);
 
-            File.WriteAllText(Path.Combine(appDataFolder, "data.json"), JsonConvert.SerializeObject(scores, Formatting.Indented));
+            foreach (var user in users)
+            {
+                user.CalculateScores();
+            }
+
+            File.WriteAllText(Path.Combine(appDataFolder, "data.json"), JsonConvert.SerializeObject(users, Formatting.Indented));
             return 0;
-        }
-
-        private static UserData GetUserData(string user)
-        {
-            var userData = new UserData { Name = user };
-
-            GetUserScore(userData);
-
-            return userData;
-        }
-
-        private static void GetUserScore(UserData userData)
-        {
-            Console.WriteLine($"Processing user '{userData.Name}'");
-            try
-            {
-                var root = XElement.Load($"https://social.msdn.microsoft.com/Profile/u/activities/feed?displayName={userData.Name}");
-                GetScoreFromRss(userData, root);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                userData.WeekScore = -1;
-            }
-        }
-
-        private static void GetScoreFromRss(UserData userData, XElement root)
-        {
-            XNamespace ns = "";
-
-            var today = DateTime.UtcNow.Date;
-            int dayOfWeek = (int)today.DayOfWeek;
-            dayOfWeek = (dayOfWeek + 1) % 7;    // We wrap the week Friday night (utc)
-            DateTime firstDayOfThisWeek = today.AddDays(-dayOfWeek);
-            DateTime firstDayOfLastWeek = firstDayOfThisWeek.AddDays(-7);
-
-            var titles = new HashSet<string>();
-
-            foreach (var item in root.Descendants(ns + "item"))
-            {
-                string pubDate = item.Descendants(ns + "pubDate").FirstOrDefault()?.Value;
-                string title = item.Descendants(ns + "title").FirstOrDefault()?.Value;
-
-                // Don't process if title is identical
-                if (titles.Contains(title)) continue;
-
-                titles.Add(title);
-
-                var dt = DateTime.Parse(pubDate);
-
-                int newScore = 0;
-
-                if (title.StartsWith("Quickly answered the question"))
-                {
-                    newScore += 20;
-                }
-                else if (title.StartsWith("Answered the question"))
-                {
-                    newScore += 15;
-                }
-                else if (title.StartsWith("Contributed a helpful post"))
-                {
-                    newScore += 5;
-                }
-                else if (title.StartsWith("Replied to a forums thread"))
-                {
-                    newScore += 1;
-                }
-
-                if (dt > firstDayOfThisWeek)
-                {
-                    userData.WeekScore += newScore;
-                }
-                else if (dt > firstDayOfLastWeek)
-                {
-                    userData.PreviousWeekScore += newScore;
-                }
-                else
-                {
-                    // Older than we process
-                    break;
-                }
-            }
-        }
-
-        class UserData
-        {
-            public string Name { get; set; }
-            public int WeekScore { get; set; }
-            public int PreviousWeekScore { get; set; }
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ForumModels
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string MSDNName { get; set; }
+        public string StackOverflowID { get; set; }
+
+        public int WeekScore { get; set; }
+        public int PreviousWeekScore { get; set; }
+
+        public void CalculateScores()
+        {
+            GetMSDNScore();
+            GetStackOverflowScore();
+        }
+
+        private void GetMSDNScore()
+        {
+            try
+            {
+                var root = XElement.Load($"https://social.msdn.microsoft.com/Profile/u/activities/feed?displayName={MSDNName}");
+                GetScoreFromRss(root);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                WeekScore = -1;
+            }
+        }
+
+        private void GetScoreFromRss(XElement root)
+        {
+            XNamespace ns = "";
+
+            var today = DateTime.UtcNow.Date;
+            int dayOfWeek = (int)today.DayOfWeek;
+            dayOfWeek = (dayOfWeek + 1) % 7;    // We wrap the week Friday night (utc)
+            DateTime firstDayOfThisWeek = today.AddDays(-dayOfWeek);
+            DateTime firstDayOfLastWeek = firstDayOfThisWeek.AddDays(-7);
+
+            var titles = new HashSet<string>();
+
+            foreach (var item in root.Descendants(ns + "item"))
+            {
+                string pubDate = item.Descendants(ns + "pubDate").FirstOrDefault()?.Value;
+                string title = item.Descendants(ns + "title").FirstOrDefault()?.Value;
+
+                // Don't process if title is identical
+                if (titles.Contains(title)) continue;
+
+                titles.Add(title);
+
+                var dt = DateTime.Parse(pubDate);
+
+                int newScore = 0;
+
+                if (title.StartsWith("Quickly answered the question"))
+                {
+                    newScore += 20;
+                }
+                else if (title.StartsWith("Answered the question"))
+                {
+                    newScore += 15;
+                }
+                else if (title.StartsWith("Contributed a helpful post"))
+                {
+                    newScore += 5;
+                }
+                else if (title.StartsWith("Replied to a forums thread"))
+                {
+                    newScore += 1;
+                }
+
+                if (dt > firstDayOfThisWeek)
+                {
+                    WeekScore += newScore;
+                }
+                else if (dt > firstDayOfLastWeek)
+                {
+                    PreviousWeekScore += newScore;
+                }
+                else
+                {
+                    // Older than we process
+                    break;
+                }
+            }
+        }
+
+        private void GetStackOverflowScore()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
